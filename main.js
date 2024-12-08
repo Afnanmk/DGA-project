da79e8643c1e997182a48dce219f36aa2fd7cd8e
Afnan Malik <Afnanmalik80@gmail.com>
2024-12-08 21:30:49 +0300

    final commit

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-// Mobile menu toggle
-let mobileMenu = document.getElementById("mobile-nav")
-mobileMenu.style.maxHeight = "0px";
-
-function toggleMenu() {
-
-    if(mobileMenu.style.maxHeight == "0px")
-    {
-        mobileMenu.style.maxHeight = "400px"
-    } else {
-        mobileMenu.style.maxHeight = "0px"
-    }
-}
-
-
-// Open and close Search bar
-const searchBtn = document.getElementById('search-btn');
-        const searchBar = document.getElementById('search-bar');
-        const closeBtn = document.getElementById('close-btn');
-
-        searchBtn.addEventListener('click', () => {
-            searchBar.style.display = 'block'; // Show the search bar
-        });
-
-        closeBtn.addEventListener('click', () => {
-            searchBar.style.display = 'none'; // Hide the search bar
-        });
-
 
 // Tabs active style
 function showTab(tabId) {
